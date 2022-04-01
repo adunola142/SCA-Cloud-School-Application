5ed28f8bae994b6dd1edbc311a95a5521ad4c13c
Tura <adunola142@gmail.com>
2022-04-01 07:49:39 +0100

    changes

--- main.java
+++ main.java
@@ -1,15 +1,8 @@
-class Main {
-
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class HelloWorld {
   public static void main(String[] args) {
-    
-    System.out.println("Enter two numbers");
-    int first = 10;
-    int second = 20;
-    
-    System.out.println(first + " " + second);
-
-    // add two numbers
-    int sum = first + second;
-    System.out.println("The sum is: " + sum);
+    System.out.println("Hello, World");
   }
 }
